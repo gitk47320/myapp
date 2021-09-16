d60141d5f1cc3fb4ce1f67259f81b94955f12a85
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-09-16 23:01:21 +0900

    bookモデル作成、seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+  User.create(name: "test#{i+1}", email: "test#{i+1}@test.com", password: 'password')
+end
+
+Book.create(title: 'テスト', description: 'テストテストテストテストテストテストテストテスト')
+Book.create(title: 'テスト', description: 'テストテストテストテストテストテストテストテスト')
+
+Book.create(title: 'テストテスト', description: 'てすとてすとてすとてすとてすとてすとてすとてすと')
+Book.create(title: 'テストテスト', description: 'てすとてすとてすとてすとてすとてすとてすとてすと')
+
+Book.create(title: '元気になれる本', description: 'この本を読むと元気になれます')
+Book.create(title: '元気になれる本', description: 'この本を読むと元気になれます')
+Book.create(title: '元気になれる本', description: 'この本を読むと元気になれます')
