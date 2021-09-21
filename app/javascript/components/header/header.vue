615ed2aea60f5089826ad0872a708bbc7d648178
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-09-22 01:09:18 +0900

    headerのガワ作成

---
+++ app/javascript/components/header/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-app>
+      <v-app-bar color="light-blue accent-3">
+        <v-app-bar-title>課題図書管理システム</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <!-- メニューリンクを書いていく -->
+      </v-app-bar>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+// min-heightを上書きしてヘッダーが縦一杯に広がるのを防ぐ
+.v-application--wrap {
+  min-height: 64px;
+}
+</style>
